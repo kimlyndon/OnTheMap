8fd6296b79b6d0887d332d07a7dcc78038b032ce
Kim Lyndon <codeCulture101@gmail.com>
2018-09-13 16:48:31 -0400

    Add ParseClient file

---
+++ On The Map/Model/Client/Parse/ParseClient.swift
@@ -0,0 +1,14 @@
+//
+//  ParseClient.swift
+//  On The Map
+//
+//  Created by Kim Lyndon on 9/13/18.
+//  Copyright © 2018 Kim Lyndon. All rights reserved.
+//
+
+import Foundation
+
+class ParseClient: NSObject {
+    
+
+}
