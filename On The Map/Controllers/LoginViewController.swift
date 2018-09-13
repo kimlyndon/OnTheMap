7200a72cbee3e465e363f2e14b690f90364c09dc
Kim Lyndon <codeCulture101@gmail.com>
2018-09-13 17:51:39 -0400

    Add MapViewController

---
+++ On The Map/Controllers/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  On The Map
+//
+//  Created by Kim Lyndon on 9/11/18.
+//  Copyright © 2018 Kim Lyndon. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ On The Map/Controllers/MapViewController.swift
@@ -0,0 +1,10 @@
+//
+//  MapViewController.swift
+//  On The Map
+//
+//  Created by Kim Lyndon on 9/13/18.
+//  Copyright © 2018 Kim Lyndon. All rights reserved.
+//
+
+import UIKit
+import MapKit
