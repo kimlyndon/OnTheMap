f0b18ed1b89c76f4900d5d79e824c6bb5393b505
kimlyndon <codeculture101@gmail.com>
2018-09-20 16:48:30 -0400

    Update UdacityConstants

--- On The Map/Model/Client/Udacity/UdacityConstants.swift
+++ On The Map/Model/Client/Udacity/UdacityConstants.swift
@@ -10,28 +10,35 @@ import Foundation
 extension UdacityClient {
     
 
-struct Constants {
+class UdacityConstants  {
         
-
-
-struct UdacityConstants {
-    static let BaseURL: String = "https://www.udacity.com/api/session"
-    static let SignUpURL: String = "https://www.udacity.com/account/auth#!/signup"
-}
-
-struct Methods {
-    static let Session = "/session"
-    static let User = "/users"
-}
+    
+    struct UdacityURL {
+        static let BaseURL: String = "https://www.udacity.com/api/session"
+        static let SignUpURL: String = "https://www.udacity.com/account/auth#!/signup"
+        static let publicUserData: String = "https://www.udacity.com/api/users"
+            
+    }
    
-struct HttpMethod {
-    static let Get = "GET"
-    static let Post = "POST"
-    static let Put = "PUT"
-    static let Delete = "DELETE"
+    struct HttpMethod {
+        static let Get = "GET"
+        static let Post = "POST"
+        static let Put = "PUT"
+        static let Delete = "DELETE"
     
     }
+    
+    struct APIResponseKeys {
+        static let account = "account"
+        static let key = "key"
+        static let user = "user"
+        static let firstName = "first_name"
+        static let lastName = "last_name"
+        
+    }
+    
+  }
 
 }
 
-}
+
