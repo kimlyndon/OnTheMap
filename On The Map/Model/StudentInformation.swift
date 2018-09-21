c67b32197a6a8f40a141001f6664bad89aeaed80
kimlyndon <codeculture101@gmail.com>
2018-09-21 15:45:28 -0400

    Refactor student array

--- On The Map/Model/StudentInformation.swift
+++ On The Map/Model/StudentInformation.swift
@@ -21,78 +21,17 @@ struct StudentInformation {
     let updatedAt: String?
     let createdAt: String?
     
-    static var studentArray = [StudentInformation]()
-    
-    init(fromDictionary dictionary: [String:AnyObject]) {
+    init?(dictionary: [String: AnyObject]) {
         
-        if let firstName = dictionary[ParseConstants.JSONResponseKeys.firstName] as? String {
-            self.firstName = firstName
-        } else {
-            self.firstName = nil
-            
-        }
-        
-        if let lastName = dictionary[ParseConstants.JSONResponseKeys.lastName] as? String {
-            self.lastName = lastName
-        } else {
-            self.lastName = nil
-            
-        }
-        
-        if let latitude = dictionary[ParseConstants.JSONResponseKeys.latitude] as? Double {
-            self.latitude = latitude
-        } else {
-            self.latitude = nil
-            
-        }
-        
-        if let longitude = dictionary[ParseConstants.JSONResponseKeys.longitude] as? Double {
-            self.longitude = longitude
-        } else {
-            self.longitude = nil
-            
-        }
-        
-        if let mapString = dictionary[ParseConstants.JSONResponseKeys.mapString] as? String {
-            self.mapString = mapString
-        } else {
-            self.mapString = nil
-            
-        }
-        
-        if let mediaURL = dictionary[ParseConstants.JSONResponseKeys.mapString] as? String {
-            self.mediaURL = mediaURL
-        } else {
-            self.mediaURL = nil
-            
-        }
-        
-        if let objectId = dictionary[ParseConstants.JSONResponseKeys.objectId] as? String {
-            self.objectId = objectId
-        } else {
-            self.objectId = nil
-            
-        }
-        
-        if let uniqueKey = dictionary[ParseConstants.JSONResponseKeys.uniqueKey] as? String {
-            self.uniqueKey = uniqueKey
-        } else {
-            self.uniqueKey = nil
-            
-        }
-        
-        if let updatedAt = dictionary[ParseConstants.JSONResponseKeys.updatedAt] as? String {
-            self.updatedAt = updatedAt
-        } else {
-            self.updatedAt = nil
-            
-        }
-        
-        if let createdAt = dictionary[ParseConstants.JSONResponseKeys.createdAt] as? String {
-            self.createdAt = createdAt
-        } else {
-            self.createdAt = nil
-            
-        }
+        self.objectId = dictionary[ParseConstants.JSONResponseKeys.ObjectId] as? String
+        self.uniqueKey = dictionary[ParseConstants.JSONResponseKeys.UniqueKey] as? String
+        self.firstName = dictionary[ParseConstants.JSONResponseKeys.FirstName] as? String
+        self.lastName = dictionary[ParseConstants.JSONResponseKeys.LastName] as? String
+        self.latitude = dictionary[ParseConstants.JSONResponseKeys.Latitude] as? Double
+        self.longitude = dictionary[ParseConstants.JSONResponseKeys.Longitude] as? Double
+        self.mapString = dictionary[ParseConstants.JSONResponseKeys.MapString] as? String
+        self.mediaURL = dictionary[ParseConstants.JSONResponseKeys.MediaURL] as? String
+        self.createdAt = dictionary[ParseConstants.JSONResponseKeys.CreatedAt] as? String
+        self.updatedAt = dictionary[ParseConstants.JSONResponseKeys.UpdatedAt] as? String
     }
 }
