58f5dfcb211c4d170344adcea5981b017f8055f1
kimlyndon <codeculture101@gmail.com>
2018-09-27 14:29:20 -0400

    Add code for GET method

--- On The Map/Model/Client/Udacity/UdacityConstants.swift
+++ On The Map/Model/Client/Udacity/UdacityConstants.swift
@@ -8,13 +8,13 @@
 
 import Foundation
 
-extension UdacityClient {
-
-
 class UdacityConstants  {
         
     
     struct UdacityURL {
+        static let ApiScheme: String = "https"
+        static let ApiHost: String = "www.udacity.com"
+        static let ApiPath: String = "/api"
         static let BaseURL: String = "https://www.udacity.com/api/session"
         static let SignUpURL: String = "https://www.udacity.com/account/auth#!/signup"
         static let PublicUserData: String = "https://www.udacity.com/api/users"
@@ -29,11 +29,26 @@ class UdacityConstants  {
     
     }
     
+    struct Methods {
+        static let Session = "/session"
+        static let User = "/users"
+        
+        
+    }
+    
+    struct APIBodyKeys {
+        static let Udacity = "udacity"
+        static let Username = "username"
+        static let Password = "password"
+        static let AccessToken = "access_token"
+        
+    }
+    
     struct APIResponseKeys {
         static let Account = "account"
         static let Registered = "registered"
         static let Session = "session"
-        static let SessionID = "id"
+        static let ID = "id"
         static let Key = "key"
         static let User = "user"
         static let FirstName = "first_name"
