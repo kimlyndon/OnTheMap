68221c64140a172c126954ccca0775f1f46dc113
kimlyndon <codeculture101@gmail.com>
2018-10-18 17:24:31 -0400

    Add properties

--- Support/AppDelegate.swift
+++ Support/AppDelegate.swift
@@ -12,7 +12,12 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    var studentLocations = [StudentLocation]()
+    var userLocation: String = ""
+    var userKey: String? = ""
+    var lastName: String? = ""
+    var firstName: String? = ""
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
