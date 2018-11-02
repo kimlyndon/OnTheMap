87e01bcec1ff606d4f073aeff5191103b9cfa35e
kimlyndon <codeculture101@gmail.com>
2018-11-02 16:47:05 -0400

    Code TableViewController

--- On The Map/Controllers/LocationInformation/TableViewController.swift
+++ On The Map/Controllers/LocationInformation/TableViewController.swift
@@ -10,7 +10,72 @@ import UIKit
 
 class TableViewController: UITableViewController {
     
-  
+    //MARK: Properties
+    var studentLocations = arrayOfStudentLocations
     
+    //MARK: Outlets
     @IBOutlet weak var ListTableView: UITableView!
+    
+    
+    //MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        refreshTableView()
+    }
+    
+    // Refresh table data
+    func refreshTableView() {
+        if let ListTableView = ListTableView {
+            ListTableView.reloadData()
+        }
+    }
+    
+    
+    //MARK: TableView delegate methods
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Get cell type
+        let cellReuseIdentifier = "TableViewCell"
+        let studentLocation = studentLocations[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as! UITableViewCell
+        
+        //Set cell defaults
+        let first = studentLocation.firstName as! String
+        let last = studentLocation.lastName as! String
+        let mediaURL = studentLocation.mediaURL as! String
+        cell.textLabel!.text = "\(first) \(last)"
+        cell.imageView!.image = UIImage(named: "icon_pin")
+        cell.detailTextLabel!.text = mediaURL
+        cell.imageView!.contentMode = UIViewContentMode.scaleAspectFit
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return studentLocations.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       
+        //Open mediaURL
+        let app = UIApplication.shared
+        let url = studentLocations[indexPath.row].mediaURL
+        
+        print("verifyURL: \(verifyUrl(urlString: url))")
+        
+        if verifyUrl(urlString: url) == true {
+            app.open(URL(string: url!)!)
+        
+        } else {
+        
+            performUIUpdatesOnMain {
+                self.createAlert(title: "Invalid URL", message: "Could not open URL.")
+            }
+        }
+    }
 }
