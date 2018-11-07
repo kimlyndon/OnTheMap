04bc78ddd523c754d97dd5a78187d18d270aab3e
kimlyndon <codeculture101@gmail.com>
2018-11-06 19:44:15 -0500

    Remove unnecessary code

--- On The Map/Model/Client/Udacity/UdacityClient.swift
+++ On The Map/Model/Client/Udacity/UdacityClient.swift
@@ -8,15 +8,6 @@
 
 import Foundation
 
-extension Optional where Wrapped == String {
-    var nilIfEmpty: String? {
-        guard let strongSelf = self else {
-            return nil
-        }
-        return strongSelf.isEmpty ? nil : strongSelf
-    }
-}
-
 class UdacityClient: NSObject{
     
     
